622afc391f30a569437befb9a00a86dd6316f818
Christophe Gagnier <admin@cgagnier.ca>
2015-08-26 12:40:03 -0400

    [#xx] Create new html helper HasActionPermission

--- LanAdeptCore/HtmlExtension/AuthorizeActionLinkExtension.cs
+++ LanAdeptCore/HtmlExtension/AuthorizeActionLinkExtension.cs
@@ -59,7 +59,7 @@ namespace LanAdeptCore.HtmlExtension
 		/// <paramparam name="showText">True to show text without a link if unauthorized</paramparam>
 		public static MvcHtmlString AuthorizeActionLink(this HtmlHelper helper, string linkText, string actionName, string controllerName, object routeValues, object htmlAttributes, bool showText = false)
 		{
-			if (HasActionPermission(helper, actionName, controllerName))
+			if (HasActionPermissionExtension.HasActionPermission(helper, actionName, controllerName))
 				return helper.ActionLink(linkText, actionName, controllerName, routeValues, htmlAttributes);
 
 			if (showText)
@@ -79,7 +79,7 @@ namespace LanAdeptCore.HtmlExtension
 		/// <paramparam name="showText">True to show text without a link if unauthorized</paramparam>
 		public static MvcHtmlString AuthorizeActionLink(this HtmlHelper helper, string linkText, string actionName, string controllerName, RouteValueDictionary routeValues, IDictionary<string, object> htmlAttributes, bool showText = false)
 		{
-			if (HasActionPermission(helper, actionName, controllerName))
+			if (HasActionPermissionExtension.HasActionPermission(helper, actionName, controllerName))
 				return helper.ActionLink(linkText, actionName, controllerName, routeValues, htmlAttributes);
 
 			if (showText)
@@ -88,75 +88,5 @@ namespace LanAdeptCore.HtmlExtension
 				return MvcHtmlString.Empty;
 		}
 
-		private static bool HasActionPermission(this HtmlHelper htmlHelper, string actionName, string controllerName)
-		{
-			ControllerBase controllerToLinkTo = null;
-			
-			if(string.IsNullOrEmpty(controllerName)) {
-				controllerToLinkTo = htmlHelper.ViewContext.Controller;
-			}
-			else {
-				controllerToLinkTo = GetControllerByName(htmlHelper, controllerName);
-			}
-
-			ControllerContext controllerContext = new ControllerContext(htmlHelper.ViewContext.RequestContext, controllerToLinkTo);
-
-			ReflectedControllerDescriptor controllerDescriptor = new ReflectedControllerDescriptor(controllerToLinkTo.GetType());
-			ActionDescriptor actionDescriptor = controllerDescriptor.FindAction(controllerContext, actionName);
-
-			return ActionIsAuthorized(controllerContext, actionDescriptor);
-		}
-
-		private static bool ActionIsAuthorized(ControllerContext controllerContext, ActionDescriptor actionDescriptor)
-		{
-			if (actionDescriptor == null)
-				return false;
-
-			AuthorizationContext authContext = new AuthorizationContext(controllerContext, actionDescriptor);
-
-			object[] controllerAttributes = authContext.Controller.GetType().GetCustomAttributes(typeof(AuthorizeAttribute), true);
-
-			foreach (AuthorizeAttribute attr in controllerAttributes)
-			{
-				attr.OnAuthorization(authContext);
-
-				if (authContext.Result != null)
-					return false;
-			}
-
-			foreach (Filter authFilter in FilterProviders.Providers.GetFilters(authContext, actionDescriptor))
-			{
-				if (authFilter.Instance is System.Web.Mvc.AuthorizeAttribute)
-				{
-					((IAuthorizationFilter)authFilter.Instance).OnAuthorization(authContext);
-
-					if (authContext.Result != null)
-						return false;
-				}
-			}
-
-			return true;
-		}
-
-		private static ControllerBase GetControllerByName(HtmlHelper helper, string controllerName)
-		{
-			var tempRequestContext = new RequestContext(helper.ViewContext.RequestContext.HttpContext, new RouteData());
-
-			IControllerFactory factory = ControllerBuilder.Current.GetControllerFactory();
-
-			IController controller = factory.CreateController(tempRequestContext, controllerName);
-
-			if (controller == null)
-			{
-				throw new InvalidOperationException(
-					string.Format(
-						CultureInfo.CurrentUICulture,
-						"Controller factory {0} controller {1} returned null",
-						factory.GetType(),
-						controllerName));
-			}
-
-			return (ControllerBase)controller;
-		}
 	}
 }
---
+++ LanAdeptCore/HtmlExtension/HasActionPermissionExtension.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace LanAdeptCore.HtmlExtension
+{
+	public static class HasActionPermissionExtension
+	{
+		/// <summary>
+		/// Determine if the current user has permission to access an action
+		/// </summary>
+		/// <param name="actionName">Action to test</param>
+		/// <returns>True if the user has access, or else false</returns>
+		public static bool HasActionPermission(this HtmlHelper htmlHelper, string actionName)
+		{
+			return HasActionPermission(htmlHelper, actionName, null);
+		}
+
+		/// <summary>
+		/// Determine if the current user has permission to access an action
+		/// </summary>
+		/// <param name="actionName">Action to test</param>
+		/// <param name="controllerName">Controller to test</param>
+		/// <returns>True if the user has access, or else false</returns>
+		public static bool HasActionPermission(this HtmlHelper htmlHelper, string actionName, string controllerName)
+		{
+			ControllerBase controllerToLinkTo = null;
+
+			if (string.IsNullOrEmpty(controllerName))
+			{
+				controllerToLinkTo = htmlHelper.ViewContext.Controller;
+			}
+			else
+			{
+				controllerToLinkTo = GetControllerByName(htmlHelper, controllerName);
+			}
+
+			ControllerContext controllerContext = new ControllerContext(htmlHelper.ViewContext.RequestContext, controllerToLinkTo);
+
+			ReflectedControllerDescriptor controllerDescriptor = new ReflectedControllerDescriptor(controllerToLinkTo.GetType());
+			ActionDescriptor actionDescriptor = controllerDescriptor.FindAction(controllerContext, actionName);
+
+			return ActionIsAuthorized(controllerContext, actionDescriptor);
+		}
+
+		private static bool ActionIsAuthorized(ControllerContext controllerContext, ActionDescriptor actionDescriptor)
+		{
+			if (actionDescriptor == null)
+				return false;
+
+			AuthorizationContext authContext = new AuthorizationContext(controllerContext, actionDescriptor);
+
+			object[] controllerAttributes = authContext.Controller.GetType().GetCustomAttributes(typeof(AuthorizeAttribute), true);
+
+			foreach (AuthorizeAttribute attr in controllerAttributes)
+			{
+				attr.OnAuthorization(authContext);
+
+				if (authContext.Result != null)
+					return false;
+			}
+
+			foreach (Filter authFilter in FilterProviders.Providers.GetFilters(authContext, actionDescriptor))
+			{
+				if (authFilter.Instance is System.Web.Mvc.AuthorizeAttribute)
+				{
+					((IAuthorizationFilter)authFilter.Instance).OnAuthorization(authContext);
+
+					if (authContext.Result != null)
+						return false;
+				}
+			}
+
+			return true;
+		}
+
+		private static ControllerBase GetControllerByName(HtmlHelper helper, string controllerName)
+		{
+			var tempRequestContext = new RequestContext(helper.ViewContext.RequestContext.HttpContext, new RouteData());
+
+			IControllerFactory factory = ControllerBuilder.Current.GetControllerFactory();
+
+			IController controller = factory.CreateController(tempRequestContext, controllerName);
+
+			if (controller == null)
+			{
+				throw new InvalidOperationException(
+					string.Format(
+						CultureInfo.CurrentUICulture,
+						"Controller factory {0} controller {1} returned null",
+						factory.GetType(),
+						controllerName));
+			}
+
+			return (ControllerBase)controller;
+		}
+	}
+}
