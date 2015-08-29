186e42201705bad3a583f52384a0ec058c329c63
Christophe Gagnier <admin@cgagnier.ca>
2015-08-29 01:18:55 -0400

    [#3] Réservation d'une place

--- LanAdept/Views/Place/ModelController/ListeModel.cs
+++ LanAdept/Views/Place/ModelController/ListeModel.cs
@@ -9,5 +9,9 @@ namespace LanAdept.Views.Place.ModelController
 	public class ListeModel
 	{
 		public IEnumerable<PlaceSection> Sections { get; set; }
+
+		public string Message { get; set; }
+
+		public string Type { get; set; }
 	}
 }
--- LanAdept/Views/Place/ModelController/PlaceController.cs
+++ LanAdept/Views/Place/ModelController/PlaceController.cs
@@ -5,11 +5,14 @@ using System.Web;
 using System.Web.Mvc;
 using LanAdept.Views.Place.ModelController;
 using LanAdeptData.DAL;
+using LanAdeptData.Model;
 
 namespace LanAdept.Controllers
 {
 	public class PlaceController : Controller
 	{
+		private const string ERROR_INVALID_ID = "Désolé, une erreur est survenue. Merci de réessayer dans quelques instants";
+
 		private UnitOfWork uow = UnitOfWork.Current;
 
 		[AllowAnonymous]
@@ -19,24 +22,29 @@ namespace LanAdept.Controllers
 		}
 
 		[AllowAnonymous]
-		public ActionResult Liste()
+		public ActionResult Liste(string message, string type)
 		{
 			ListeModel listeModel = new ListeModel();
 
+			listeModel.Message = message;
+			listeModel.Type = type;
 			listeModel.Sections = uow.PlaceSectionRepository.Get();
 
 			return View(listeModel);
 		}
 
 		[Authorize]
-		public ActionResult Place(int? id)
+		public ActionResult Reserver(int? id)
 		{
-			if (id == null)
-				return RedirectToAction("Liste");
+			if (id == null || id < 1)
+				return RedirectToAction("Liste", new { message = ERROR_INVALID_ID, type = "erreur" });
 
+			Place placeAReserver = uow.PlaceRepository.GetByID(id.Value);
 
+			if(placeAReserver == null)
+				return RedirectToAction("Liste", new { message = ERROR_INVALID_ID, type = "erreur" });
 
-			return View();
+			return RedirectToAction("Liste", new { message = "La place " + placeAReserver.PlaceSection.Name + placeAReserver.Number.ToString("00") + " a été réservée (SIMUL)", type = "succes" });
 		}
 	}
 }
