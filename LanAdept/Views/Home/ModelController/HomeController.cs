5114bca02c67df430be98d97a4ef7c5ac1667453
IceSentry <c.giguere42@gmail.com>
2015-08-29 23:07:25 -0400

    [#6] Page d'accueil

--- LanAdept/Views/Home/ModelController/HomeController.cs
+++ LanAdept/Views/Home/ModelController/HomeController.cs
@@ -9,22 +9,22 @@ using LanAdeptData.Model;
 
 namespace LanAdept.Controllers
 {
-    [AllowAnonymous]
-    public class HomeController : Controller
-    {
-        UnitOfWork uow = new UnitOfWork();
+	[AllowAnonymous]
+	public class HomeController : Controller
+	{
+		UnitOfWork uow = new UnitOfWork();
 
-        public ActionResult Index()
-        {
-            String date = "2015-10-14";
-            DateTime dateLan = DateTime.Parse(date);
-            dateLan = dateLan.AddHours(12);
+		public ActionResult Index()
+		{
+			String date = "2015-10-14";
+			DateTime dateLan = DateTime.Parse(date);
+			dateLan = dateLan.AddHours(12);
 			dateLan = TimeZoneInfo.ConvertTimeToUtc(dateLan);
 
-            double dateLanMs = dateLan.Subtract(new DateTime(1970, 1, 1, 0, 0, 0)).TotalMilliseconds; //Donne le temps en unix time
+			double dateLanMs = dateLan.Subtract(new DateTime(1970, 1, 1, 0, 0, 0)).TotalMilliseconds; //Donne le temps en unix time
 
-            ViewBag.dateLan = dateLanMs;
-            return View();
-        }
-    }
+			ViewBag.dateLan = dateLanMs;
+			return View();
+		}
+	}
 }
