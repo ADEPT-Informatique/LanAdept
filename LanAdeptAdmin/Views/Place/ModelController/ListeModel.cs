0f84ee762e284c1c04b24e3d0141587e6dadccf9
island212 <islandx424@gmail.com>
2015-09-19 00:04:26 -0400

    Add Details and Remove ModularView

--- LanAdeptAdmin/Views/Place/ModelController/ListeModel.cs
+++ LanAdeptAdmin/Views/Place/ModelController/ListeModel.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using LanAdeptData.Model;
 
-namespace LanAdept.Views.Place.ModelController
+namespace LanAdeptAdmin.Views.Place.ModelController
 {
 	public class ListeModel
 	{
--- LanAdeptAdmin/Views/Place/ModelController/PlaceController.cs
+++ LanAdeptAdmin/Views/Place/ModelController/PlaceController.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using LanAdept.Views.Place.ModelController;
+using LanAdeptAdmin.Views.Place.ModelController;
 using LanAdeptCore.Service;
 using LanAdeptCore.Service.ServiceResult;
 using LanAdeptData.DAL;
 using LanAdeptData.Model;
 
-namespace LanAdept.Controllers
+namespace LanAdeptAdmin.Controllers
 {
 	public class PlaceController : Controller
 	{
@@ -23,15 +23,38 @@ namespace LanAdept.Controllers
 			return RedirectToAction("Liste");
 		}
 
-		[AllowAnonymous]
-		public ActionResult Liste()
-		{
-			ListeModel listeModel = new ListeModel();
+        [AllowAnonymous]
+        public ActionResult Liste()
+        {
+            ListeModel listeModel = new ListeModel();
 
-			listeModel.Sections = uow.PlaceSectionRepository.Get();
+           listeModel.Sections = uow.PlaceSectionRepository.Get();
 
-			return View(listeModel);
-		}
+            return View(listeModel);
+        }
+
+
+        [Authorize]
+        public ActionResult Details(int? id)
+        {
+            if (id == null || id < 1)
+            {
+                TempData["Error"] = ERROR_INVALID_ID;
+                return RedirectToAction("Liste");
+            }
+
+            Place placeAReserver = uow.PlaceRepository.GetByID(id.Value);
+
+            if (placeAReserver == null)
+            {
+                TempData["Error"] = ERROR_INVALID_ID;
+                return RedirectToAction("Liste");
+            }
+
+            Place place = uow.PlaceRepository.GetByID(id);
+
+            return View(place);
+        }
 
 		[Authorize]
 		public ActionResult Reserver(int? id)
@@ -61,20 +84,6 @@ namespace LanAdept.Controllers
 			}
 
 			return RedirectToAction("Liste");
-		}
-
-		[Authorize]
-		public ActionResult MaPlace()
-		{
-			if (!PlaceService.HasUserPlace())
-			{
-				TempData["Error"] = "Vous n'avez pas encore réservé une place.";
-				return RedirectToAction("Liste");
-			}
-
-			Reservation reservation = UserService.GetLoggedInUser().LastReservation;
-
-			return View(reservation);
 		}
 
 		[Authorize]
