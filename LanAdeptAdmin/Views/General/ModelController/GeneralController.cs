56bd7f62f5d9b3b4672250e460c4ab0ed7bfaf03
Christophe Gagnier <admin@cgagnier.ca>
2015-09-19 22:29:31 -0400

    [#7] Paramètres généraux du LAN

---
+++ LanAdeptAdmin/Views/General/ModelController/GeneralController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LanAdeptAdmin.Views.General.ModelController;
+using LanAdeptData.DAL;
+using LanAdeptData.Model;
+
+namespace LanAdeptAdmin.Controllers
+{
+    public class GeneralController : Controller
+    {
+		UnitOfWork uow = UnitOfWork.Current;
+
+		[Authorize]
+		public ActionResult Index()
+		{
+			return RedirectToAction("Settings");
+		}
+
+		[Authorize]
+		public ActionResult Settings()
+		{
+			Setting settings = uow.SettingRepository.GetCurrentSettings();
+			SettingsModel settingsModel = new SettingsModel(settings);
+
+			return View(settingsModel);
+		}
+	}
+}
