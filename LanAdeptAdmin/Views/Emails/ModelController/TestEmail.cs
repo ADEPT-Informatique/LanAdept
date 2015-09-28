ee23b94d5351faeb7013eb447d5804f1ac176935
Christophe Gagnier <admin@cgagnier.ca>
2015-09-27 20:53:57 -0400

    [#08] Emails

---
+++ LanAdeptAdmin/Views/Emails/ModelController/TestEmail.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Postal;
+
+namespace LanAdeptAdmin.Emails
+{
+	public class TestEmail : Email
+	{
+		public string To { get; set; }
+		public string Message { get; set; }
+	}
+}
