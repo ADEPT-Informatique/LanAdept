7536e187d577e007d9c0163ca3a36095c093bc75
Christophe Gagnier <admin@cgagnier.ca>
2015-10-04 22:14:36 -0400

    [#08] Administration des utilisateurs

---
+++ LanAdeptAdmin/Views/User/ModelController/UserController.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using LanAdeptData.DAL;
+using LanAdeptData.Model;
+
+namespace LanAdeptAdmin.Controllers
+{
+    public class UserController : Controller
+    {
+		UnitOfWork uow = UnitOfWork.Current;
+
+		[Authorize]
+        public ActionResult Index()
+        {
+			var users = uow.UserRepository.Get();
+            return View(users.ToList());
+        }
+
+		[Authorize]
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = uow.UserRepository.GetByID(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+		[Authorize]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+			User user = uow.UserRepository.GetByID(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+			ViewBag.RoleID = new SelectList(uow.RoleRepository.Get(), "RoleID", "Name", user.RoleID);
+            return View(user);
+        }
+
+        [HttpPost]
+		[Authorize]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "UserID,Email,Password,Salt,CompleteName,RoleID")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+				uow.UserRepository.Update(user);
+				uow.Save();
+                return RedirectToAction("Index");
+            }
+			ViewBag.RoleID = new SelectList(uow.RoleRepository.Get(), "RoleID", "Name", user.RoleID);
+            return View(user);
+        }
+
+		[Authorize]
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+			User user = uow.UserRepository.GetByID(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+		[Authorize]
+        public ActionResult DeleteConfirmed(int id)
+        {
+			User user = uow.UserRepository.GetByID(id);
+			uow.UserRepository.Delete(user);
+			uow.Save();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
