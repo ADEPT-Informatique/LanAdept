3e7f99a638fce199f9bd0ea733f93a0d588bc424
IceSentry <c.giguere42@gmail.com>
2015-10-05 23:32:27 -0400

    [#2] Administration tournois

---
+++ LanAdeptAdmin/Views/Tournaments/ModelController/EmailModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LanAdeptAdmin.Views.Tournaments.ModelController
+{
+	public class EmailModel
+	{
+		public string content { get; set; }
+	}
+}
