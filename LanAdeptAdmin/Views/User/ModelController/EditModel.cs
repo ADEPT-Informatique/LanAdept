ac0df46c20675afd31cc89e3638ebfd89dd59028
Christophe Gagnier <admin@cgagnier.ca>
2015-10-05 20:38:41 -0400

    Admin des users

---
+++ LanAdeptAdmin/Views/User/ModelController/EditModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LanAdeptData.Model;
+using LanAdeptData.Validation;
+
+namespace LanAdeptAdmin.Views.Users.ModelController
+{
+	public class EditModel
+	{
+		public int UserID { get; set; }
+
+		[UniqueEmail]
+		[Editable(false)]
+		public string Email { get; set; }
+
+		[Required]
+		public string CompleteName { get; set; }
+
+		[Required]
+		public int RoleID { get; set; }
+
+		public SelectList RoleList { get; set; }
+
+		public EditModel() { }
+		public EditModel(User user)
+		{
+			Email = user.Email;
+			CompleteName = user.CompleteName;
+			RoleID = user.RoleID;
+		}
+
+	}
+}
