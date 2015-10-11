4333692f591ffeba4d21fdf8c1e5bd44f2de3033
Christophe Gagnier <admin@cgagnier.ca>
2015-10-11 01:24:30 -0400

    Fixes #16

--- LanAdeptAdmin/Scripts/scanner.js
+++ LanAdeptAdmin/Scripts/scanner.js
@@ -5,26 +5,14 @@
 		if (e.which >= 48 && e.which <= 57) {
 			chars.push(String.fromCharCode(e.which));
 		}
-		//console.log(e.which + ":" + chars.join("|"));
 		if (pressed == false) {
 			setTimeout(function () {
+				console.log("Test: " + chars.join(""));
 				if (chars.length >= 8) {
 					var barcode = chars.join("");
 					console.log("Barcode Scanned: " + barcode);
 
-					var form = $('<form></form>');
-					form.attr("method", "post");
-					form.attr("action", "/place/search/");
-
-					var field = $('<input></input>');
-					field.attr("type", "hidden");
-					field.attr("name", "Query");
-					field.attr("value", barcode);
-
-					form.append(field);
-					$(document.body).append(form);
-
-					form.submit();
+					window.location = "/Place/Search/?Query=" + barcode;
 				}
 				chars = [];
 				pressed = false;
